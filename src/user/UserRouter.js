7b03ae205433719fa4ed283c3edaf6e1f1bb9f46
richfjiq <rfjiq1986@gmail.com>
2024-01-04 13:50:45 -0600

    chore: add validator middleware

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -3,14 +3,33 @@ const express = require('express');
 const UserService = require('./UserService');
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
+const validateUsername = (req, res, next) => {
   const { username } = req.body;
   if (!username) {
-    return res.status(400).send({
-      validationErrors: {
-        username: 'Username cannot be null.',
-      },
-    });
+    req.validateErrors = {
+      username: 'Username cannot be null.',
+    };
+  }
+  next();
+};
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+    req.validateErrors = {
+      ...req.validateErrors,
+      email: 'E-mail cannot be null.',
+    };
+  }
+  next();
+};
+
+router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res) => {
+  if (req.validateErrors) {
+    const response = {
+      validationErrors: { ...req.validateErrors },
+    };
+    return res.status(400).send(response);
   }
   await UserService.save(req.body);
   return res.send({ message: 'User created' });
