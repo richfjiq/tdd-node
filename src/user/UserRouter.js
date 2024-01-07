24103e39a026b088149a9c9aec055d0dcadb36d7
richfjiq <rfjiq1986@gmail.com>
2024-01-07 17:34:50 -0600

    chore: adds tests for remaining validations

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -27,9 +27,23 @@ const router = express.Router();
 
 router.post(
   '/api/1.0/users',
-  check('username').notEmpty().withMessage('Username cannot be null.'),
-  check('email').notEmpty().withMessage('E-mail cannot be null.'),
-  check('password').notEmpty().withMessage('Password cannot be null.'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null.')
+    .bail()
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have min 4 and max 32 characters.'),
+  check('email').notEmpty().withMessage('E-mail cannot be null.').bail().isEmail().withMessage('E-mail is not valid.'),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null.')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters.')
+    .bail()
+    .matches(/^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*)$/)
+    .withMessage('Password must have at least 1 uppercase, 1 lowercase letter and 1 number.'),
+
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
