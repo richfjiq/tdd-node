3c6f2d1f73942b964bc84d192699917cf791acf6
richfjiq <rfjiq1986@gmail.com>
2024-02-18 18:30:30 -0600

    chore: adds error body

--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/app');
-const sequelize = require('./src/config/database');
-
-sequelize.sync({ force: true });
-
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(3000, () => console.log('App is running :)'));
-}
